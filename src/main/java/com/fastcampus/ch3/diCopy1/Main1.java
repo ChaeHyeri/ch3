5eaaff4c4bf08e8ebac343431dc26790b2dc5fb3
ChaeHyeri <ricin_@naver.com>
2022-11-10 18:39:04 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -0,0 +1,28 @@
+package com.fastcampus.ch3.diCopy1;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+class Car {}
+class SportsCar extends Car{}
+class Truck extends Car{}
+class Engine {}
+
+public class Main1 {
+    public static void main(String[] args) throws Exception {
+        Car car = (Car)getObject("car");    // getObject()의 반환타입이 Object이므로 형변환 필요
+        Engine engine = (Engine)getObject("engine");
+        System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
+    }
+
+    static Object getObject(String key) throws Exception  {
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty(key));
+
+        return clazz.newInstance();
+
+    }
+}
